700e8e4a8a8133057a66c33b59806d9ba85b2f88
lldpwr <lldpwr@gmail.com>
2020-11-02 23:09:51 -0500

    simplify test

--- site/test.ps1
+++ site/test.ps1
@@ -1,9 +1,13 @@
+test-path ./launch.ps1
+<#
 $job = Start-Job -ScriptBlock {./launch.ps1}
-Start-Sleep 1
+Start-Sleep 4s
 $JobOutput =  $job | Receive-Job
 Write-Host $JobOutput
-Start-Sleep 1
-$job | Stop-Job -Force
+Start-Sleep 1s
+# hang here
+$job | Stop-Job
 $job | Remove-Job -Force
+#>
 
 
